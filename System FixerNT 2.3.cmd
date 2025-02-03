ab94f7bf86ccb67e07f468f3e9267fab697d981b
Sagar Mondal <128616069+dhsagaryt@users.noreply.github.com>
2025-02-04 02:16:21 +0530

    Update System FixerNT 2.3.cmd

--- System FixerNT 2.3.cmd
+++ System FixerNT 2.3.cmd
@@ -7,3 +7,11 @@ Set "proName=System FixerNT"
 Set "proVer=2.3"
 
 title %proName% %proVer%
+
+:: Check if script is run as Administrator
+net session >nul 2>&1
+if %errorLevel% NEQ 0 (
+    echo This program must be run as Administrator!
+    pause
+    exit
+)
