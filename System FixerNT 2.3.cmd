7998d3806bf6ddec3bfa963375f54a82aa6137c7
Sagar Mondal <128616069+dhsagaryt@users.noreply.github.com>
2025-02-04 02:20:12 +0530

    Update System FixerNT 2.3.cmd

--- System FixerNT 2.3.cmd
+++ System FixerNT 2.3.cmd
@@ -111,3 +111,134 @@ IF ERRORLEVEL ==2 GOTO Opt6_Menu
 IF ERRORLEVEL ==1 GOTO Opt5_Menu
 goto MoreOpt
 
+
+
+REM =====================================
+REM	== OPTION 1 ==========================
+REM =====================================
+:Opt1_Menu
+cls
+echo ╔══════════════════════════════════════════╗
+echo ║    [1] System File Checker               ║
+echo ╠══════════════════════════════════════════╣
+echo ║ [A] Scan and Repair All System Files     ║
+echo ║ [B] Just Scan Without Repairing          ║
+echo ║ [C] Specific System File Repair          ║
+echo ║ [D] Specific System File Scan Only       ║
+echo ║ [E] Repair System Files from a Backup    ║
+echo ║ [F] Log Results of SFC Scan              ║
+echo ║ [N] Previous Menu                        ║
+echo ╠══════════════════════════════════════════╣
+CHOICE /N /C:ABCDEFN /M "║ Enter your choice:"%1
+echo ╚══════════════════════════════════════════╝
+
+:: Handle user input
+IF ERRORLEVEL ==7 GOTO homWel
+IF ERRORLEVEL ==6 GOTO Opt1f
+IF ERRORLEVEL ==5 GOTO Opt1e
+IF ERRORLEVEL ==4 GOTO Opt1d
+IF ERRORLEVEL ==3 GOTO Opt1c
+IF ERRORLEVEL ==2 GOTO Opt1b
+IF ERRORLEVEL ==1 GOTO Opt1a
+goto Opt1_Menu
+
+REM =====================================
+REM	== OPTION 1A =========================
+:Opt1a
+cls
+echo.
+sfc /scannow
+
+::Check Solve OR Not?
+if %errorLevel% NEQ 0 (
+    echo SFC detected issues but couldn't fix them. Try DISM.
+	:dismChoice
+	echo.
+    CHOICE /N /C:YN /M "Would you like to Use DISM? [Y/N]:"%1
+	IF ERRORLEVEL ==2 GOTO Opt1_Menu
+	IF ERRORLEVEL ==1 GOTO dismChoiceDO
+    goto dismChoice
+	
+	:dismChoiceDO
+	DISM /Online /Cleanup-Image /RestoreHealth
+	
+	:sfcChoice
+	echo.
+    CHOICE /N /C:YN /M "Would you like to Try Again SFC? [Y/N]:"%1
+	IF ERRORLEVEL ==2 GOTO Opt1_Menu
+	IF ERRORLEVEL ==1 GOTO Opt1a
+    goto sfcChoice
+)
+
+::IF SFC DONE, THEN!
+pause
+goto Opt1_Menu
+
+
+REM =====================================
+REM	== OPTION 1B =========================
+:Opt1b
+cls
+echo.
+sfc /verifyonly
+
+::IF verifyonly DONE, THEN!
+pause
+goto Opt1_Menu
+
+
+REM =====================================
+REM	== OPTION 1C =========================
+:Opt1c
+cls
+echo.
+echo Example: C:\Windows\System32\example.dll
+echo.
+set /p filepath=Enter full file path: 
+sfc /scanfile=%filepath%
+
+::IF scanfile DONE, THEN!
+pause
+goto Opt1_Menu
+
+
+REM =====================================
+REM	== OPTION 1D =========================
+:Opt1d
+cls
+echo.
+echo Example: C:\Windows\System32\example.dll
+echo.
+set /p filepath=Enter full file path: 
+sfc /verifyfile=%filepath%
+
+::IF verifyfile DONE, THEN!
+pause
+goto Opt1_Menu
+
+
+REM =====================================
+REM	== OPTION 1E =========================
+:Opt1e
+cls
+echo.
+echo Currently this Option Not Available for Your System
+
+::IF NOT DONE, THEN!
+pause
+goto Opt1_Menu
+
+
+REM =====================================
+REM	== OPTION 1F =========================
+:Opt1f
+cls
+echo.
+echo Displaying SFC Logs...
+findstr /c:"[SR]" %windir%\Logs\CBS\CBS.log > "%userprofile%\Desktop\SFC_Log.txt"
+echo Logs saved to Desktop as SFC_Log.txt
+notepad "%userprofile%\Desktop\SFC_Log.txt"
+
+::IF LogsFile DONE, THEN!
+pause
+goto Opt1_Menu
